0ca1fbe53c47af4d9b8b160f5bc5ade1e827cec5
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2022-02-23 11:44:21 -0300

    Finalizando

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsumoAPI.Models;
+using Newtonsoft.Json;
+
+Console.WriteLine("Informe seu CEP: ");
+var cep = Console.ReadLine();
+Console.WriteLine("");
+
+var client = new HttpClient
+{
+    BaseAddress = new System.Uri("https://viacep.com.br/")
+};
+
+var response = client.GetAsync($"ws/{cep}/json").Result;
+
+if (response.IsSuccessStatusCode)
+{
+    Endereco endereco = JsonConvert.DeserializeObject<Endereco>(response.Content.ReadAsStringAsync().Result) ?? new Endereco();
+    Console.WriteLine(endereco.ToString());
+}
